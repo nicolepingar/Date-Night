46979550206e78b9f6e9c381c8584bdee5c71aec
yinzhedy <yinzhe.dy@gmail.com>
2021-09-22 10:47:24 -0400

    started adding in pseudocoding for js to describe some to do tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 var videoGameApiKey = "8e16f8ff07d448cca1ccbdac1846964d"
+
+// fetch video game Api data and store in local storage
+
+// fetch drink Api data and store in local storage
+
+// fetch recipe/restaurant api data and store in local storage
+
+// function to change display properties of cards from hidden to visible 
+
