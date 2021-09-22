03d11d53ef6d262664ceae665509097dd158f723
Nicole Pingar <nicolepingar@yahoo.com>
2021-09-22 14:30:46 -0400

    js function and html work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 var videoGameApiKey = "8e16f8ff07d448cca1ccbdac1846964d"
 var videoGame = "https://api.rawg.io/api/games?key=8e16f8ff07d448cca1ccbdac1846964d"
+var glassSelection = $(".glass-select")
+var mealSelect = $(".meal-select")
+var cuisineSelect = $(".cuisine-select")
+var genreSelect = $(".genre-select")
+var formSubmitButton = $(".button")
 
+
+
+console.log(glassSelection.val());
+console.log(mealSelect.val());
+console.log(cuisineSelect.val());
+console.log(genreSelect.val());
 // fetching full games list database
 
 
@@ -14,6 +25,101 @@ var videoGame = "https://api.rawg.io/api/games?key=8e16f8ff07d448cca1ccbdac18469
 // fetch recipe/restaurant api data and store in local storage
 
 // function to change display properties of cards from hidden to visible 
+
+
+
+function FormSubmit(event) {
+    event.preventDefault();
+    var glass = glassSelection.val();
+    var meal = mealSelect.val()
+    var cuisine = cuisineSelect.val();
+    var genre = genreSelect.val();
+
+    selectionValues(glass, meal, cuisine, genre)
+
+}
+
+function selectionValues(glass, meal, cuisine, genre) {
+    var drinkDB = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g"
+    function getDrink(drinkDB) {
+        fetch(drinkDB)
+            .then(function (response) {
+
+                return response.json();
+            })
+            .then(function (data) {
+                for (var i = 0; i < data.drinks.length; i++) {
+                    console.log(data.drinks[i].strGlass);
+                }
+            })
+    }
+    getDrink(drinkDB);
+}
+
+
+formSubmitButton.on("click", FormSubmit)
+
+
+
+
+// var mealDB = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+// function getMeal(mealDB) {
+//     fetch(mealDB)
+//         .then(function (response) {
+
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+
+//             // for (var i = 0; i < data.meals.length; i++) {
+//             //     console.log(data.meals[i].strArea);
+//             // }
+//         })
+// }
+// getMeal(mealDB);
+
+
+
+
+// var videoGame = "https://api.rawg.io/api/genres?key=8e16f8ff07d448cca1ccbdac1846964d"
+// function getVid(videoGame) {
+//     fetch(videoGame)
+//         .then(function (response) {
+
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+
+//             for (var i = 0; i < data.results.length; i++) {
+//                 console.log(data.results[i].name);
+//             }
+
+//         })
+// }
+// getVid(videoGame);
+
+
+
+
+
+
+
+// var glassOptions = [
+//     "Cocktail glass",
+//     "Shot glass",
+//     "Martini Glass",
+//     "Highball Glass",
+//     "Collins Glass",
+//     "Old-fashioned glass",
+//     "Whiskey sour glass",
+//     "Champagne Flute",
+//     "Margarita glass",
+//     "Beer pilsner",
+//     "Pint glass",
+//     "Coupe Glass",
+//     "Beer mug"]
 
 // var mealOptions = [
 //     "Beef",
@@ -35,69 +141,3 @@ var videoGame = "https://api.rawg.io/api/games?key=8e16f8ff07d448cca1ccbdac18469
 //     "Italian",
 //     "Dutch",
 // ]
-
-// var mealDB = "https://www.themealdb.com/api/json/v1/1/search.php?f=s"
-// function getMeal(mealDB) {
-//     fetch(mealDB)
-//         .then(function (response) {
-
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             // console.log(data);
-
-//             for (var i = 0; i < data.meals.length; i++) {
-//                 console.log(data.meals[i].strArea);
-//             }
-//         })
-// }
-// getMeal(mealDB);
-
-// var glassOptions = [
-//     "Cocktail glass",
-//     "Shot glass",
-//     "Martini Glass",
-//     "Highball Glass",
-//     "Collins Glass",
-//     "Old-fashioned glass",
-//     "Whiskey sour glass",
-//     "Champagne Flute",
-//     "Margarita glass",
-//     "Beer pilsner",
-//     "Pint glass",
-//     "Coupe Glass",
-//     "Beer mug"]
-// var drinkDB = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g"
-// function getDrink(drinkDB) {
-//     fetch(drinkDB)
-//         .then(function (response) {
-
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             for (var i = 0; i < data.drinks.length; i++) {
-//                 console.log(data.drinks[i].strGlass);
-//             }
-//         })
-// }
-// getDrink(drinkDB);
-
-var videoGame = "https://api.rawg.io/api/genres?key=8e16f8ff07d448cca1ccbdac1846964d"
-function getVid(videoGame) {
-    fetch(videoGame)
-        .then(function (response) {
-
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-
-            for (var i = 0; i < data.results.length; i++) {
-                console.log(data.results[i].name);
-            }
-
-        })
-}
-getVid(videoGame);
-
-
