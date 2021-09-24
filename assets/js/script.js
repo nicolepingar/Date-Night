81baa5e28e9ff5f089c60d4f28e314de6d1c99a6
Nicole Pingar <nicolepingar@yahoo.com>
2021-09-24 15:02:41 -0400

    js comments and readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// global variables 
 var videoGameApiKey = "8e16f8ff07d448cca1ccbdac1846964d"
 var videoGame = "https://api.rawg.io/api/games?key=8e16f8ff07d448cca1ccbdac1846964d"
 var glassSelection = $(".glass-select")
@@ -7,52 +8,57 @@ var formSubmitButton = $(".submit-button")
 var drinkResult = $(".drink-result")
 var mealResult = $(".meal-result")
 var gameResult = $(".game-result")
-
+// when the form is submitted, the values are set as params in sectionValues function 
 function FormSubmit(event) {
     event.preventDefault();
-    var glass = glassSelection.val().replace(" ", "_");
+    var glass = glassSelection.val().replace(" ", "_"); // spaces are replaced with an underscore because api url format
     var meal = mealSelect.val();
-    var genre = genreSelect.val().replace(" ", "-");
+    var genre = genreSelect.val().replace(" ", "-"); // spaces are replaced with a dash because api url format
     selectionValues(glass, meal, genre);
 }
-
-
+// function passes through all the dropbox values and creates card values 
 function selectionValues(glass, meal, genre) {
-    $(".glass-select option:eq(0)").prop("selected", true);
-    $(".meal-select option:eq(0)").prop("selected", true);
-    $(".genre-select option:eq(0)").prop("selected", true);
+    formSubmitButton.text("Reroll") // when form is submitted, button text changes to reroll with dice
+    var dice =$("<i>").addClass("fas fa-dice")
+    formSubmitButton.append(dice)
     var drinkDB = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=" + glass
     function getDrink(drinkDB) {
-        localStorage.setItem("glassSelectValue", glass);
+        localStorage.setItem("glassSelectValue", glass); // sets drink input values to local storage 
         fetch(drinkDB)
             .then(function (response) {
                 return response.json();
             })
-            .then(function (data) {
-                console.log(data);
-                var indexDrink = Math.floor(Math.random() * data.drinks.length)
+            .then(function (data) { 
+                // var drinkID = data.drinks[indexDrink].idDrink
+                // var drinkIDUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkID
+                // fetch(drinkIDUrl)
+                // .then(function (response) {
+                //     return response.json();
+                // })
+                // .then(function (data) {
+                //     console.log(data);
+                // random drink name and picture generation function 
+                var indexDrink = Math.floor(Math.random() * data.drinks.length) // random number based on number of drinks in data 
                 var drinkName = data.drinks[indexDrink].strDrink
-                // $(".drink-result-name").text('')
                 $(".drink-result-name").text(drinkName)
-                localStorage.setItem("generatedDrink", drinkName);
+                localStorage.setItem("generatedDrink", drinkName); // sets drink name to local storage 
                 var drinkPic = data.drinks[indexDrink].strDrinkThumb
                 $(".drink-pic").attr({ src: drinkPic, id: "drinkId", width: 150, height: 150 })
             })
     }
     var mealDB = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + meal
     function getMeal(mealDB) {
-        localStorage.setItem("mealSelectValue", meal);
+        localStorage.setItem("mealSelectValue", meal); // sets meal input values to local storage 
         fetch(mealDB)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
-                var indexMeal = Math.floor(Math.random() * data.meals.length)
+                // random meal name, picture and recipe link generation function 
+                var indexMeal = Math.floor(Math.random() * data.meals.length) // random number based on number of meals in data 
                 var mealID = data.meals[indexMeal].idMeal
-                console.log(mealID);
                 var mealIDUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealID
-                fetch(mealIDUrl) // second fetch for new api
+                fetch(mealIDUrl) // second fetch for new api based on meal id number
                     .then(function (response) {
                         return response.json();
                     })
@@ -60,12 +66,11 @@ function selectionValues(glass, meal, genre) {
                         console.log(data);
                         var mealName = data.meals[0].strMeal
                         $(".meal-result-name").text(mealName)
-                        localStorage.setItem("generatedMeal", mealName);
-                        $(".meal-recipe").text("Please click the picture for the full recipe.")
+                        localStorage.setItem("generatedMeal", mealName); // sets meal name to local storage 
+                        $(".meal-recipe").text("Please click the picture for the full recipe.") // recipe url is linked in picture
                         var mealPic = data.meals[0].strMealThumb
                         $(".meal-pic").attr({ src: mealPic, id: "mealId", width: 150, height: 150 })
                         var recipeLink = data.meals[0].strSource
-                        // var mealPicEl = $("<img>", { src: mealPic, id: "mealId", width: 150, height: 150 })
                         $(".a-tag-recipe").attr("href", recipeLink)
                         $(".a-tag-recipe").attr("target", "_blank")
                     })
@@ -73,32 +78,30 @@ function selectionValues(glass, meal, genre) {
     }
     var videoGame = "https://api.rawg.io/api/games?key=8e16f8ff07d448cca1ccbdac1846964d&genres=" + genre.toLowerCase();
     function getVid(videoGame) {
-        localStorage.setItem("genreSelectValue", genre);
+        localStorage.setItem("genreSelectValue", genre); // sets game input values to local storage 
         fetch(videoGame)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
-                var indexGames = Math.ceil(Math.random() * 19)
+                // random game name and picture generation function 
+                var indexGames = Math.ceil(Math.random() * 19) // random number based on the amount of games that are available (19)
                 var gameName = data.results[indexGames].name;
                 $(".game-result-name").text(gameName)
-                localStorage.setItem("generatedGame", gameName);
+                localStorage.setItem("generatedGame", gameName); // sets game name to local storage 
                 var gamePic = data.results[indexGames].background_image;
                 $(".game-pic").attr({ src: gamePic, id: "gameId", width: 150, height: 150 })
             })
     }
+    // calls each function on click of form submit 
     getDrink(drinkDB);
     getMeal(mealDB);
     getVid(videoGame);
     return;
-
 }
 formSubmitButton.on("click", FormSubmit)
-
-// card js THIS
+// card flip function 
 const card = $(".card__container");
-
 card.on('click', '.card__inner', function () {
     $(this).toggleClass('is-flipped');
 })
